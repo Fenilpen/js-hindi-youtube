d6e7f9a62fed979ad625fa0c8bdc6d0a1b40acc0
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2025-01-12 11:08:37 +0000

    scope

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,61 @@
+// let me learn about scopes
+
+// case 1
+
+let a = 10
+const b = 20
+var c = 30  // this is global
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// this works completly fine
+// but the problem will start accours in this 
+
+// {} <=this is scope 
+// scope in objects are diffrent they are object decleretion
+// scope comes with if , functions they are called scope
+
+// case 2
+
+if (true){
+    let a = 1
+    const b = 2
+    var c = 3
+    
+    console.log("inner",a);
+    console.log("inner",b);
+    console.log("inner",c);
+    
+} // this is scope
+
+// let, const will follow scopes but var will not
+
+console.log("outer",a);
+console.log("outer",b);
+console.log("outer",c);
+
+// know why var is cusing trouble var only follow global scope
+// Variables declared inside a block (like within {}) 
+// can only be accessed within that block. but in var the story is diffrent
+// var dont follow any scope he is goon 
+
+// more explaination and diffrence bitween globle scope and local scope 
+
+// Global Scope:
+// Variables or functions declared outside of any function or block have global scope.
+// These can be accessed from anywhere in your code.
+// It's like writing something on a big whiteboard that everyone in the room can see.
+
+// Local Scope:
+// Variables or functions declared inside a function have local scope (function scope).
+// These can only be accessed within that specific function.
+// It's like writing something in your personal diary, which only you can read.
+
+// block scope: 
+// introduced with let and const in ES6 (modern JavaScript). 
+// Variables declared inside a block (like within {}) can only be accessed within that block.
+
+// special note
+// scope in node and console will be diffrent
