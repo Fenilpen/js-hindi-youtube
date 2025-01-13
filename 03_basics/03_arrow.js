f2ca3c2fb5f3cb289da40abdc440c3b78f26c209
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2025-01-13 12:07:45 +0000

    arrow

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,45 @@
+// in this time let me learn about this keyword
+// this refers current context
+
+// const user = {
+//     username: "fenil",
+//     price : 890,
+
+//     welcome: function(){
+//      console.log( `${this.username} ,welcome to website`);
+//      console.log(this);
+//     }
+// }
+
+// user.welcome()
+// user.username = "fen"
+// user.welcome()
+
+// console.log(this);
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// basic arrow function // 
+
+// let gaal = (val1,val2) => {
+//   return val1 + val2
+// }
+
+// let var1 = gaal(1,2)
+// console.log(var1);
+
+
+// implisit return (no need to type return) 
+// if you use curly breses so return type will be nassesery
+
+// let gaal = (val1,val2) => val1 + val2
+
+// console.log(gaal(2,3));
+
+
+
+// how to return object ({})
+
+// let newfun = () => ({FENIL:"ENTREPRENUR"}) 
+
+// console.log(newfun());
