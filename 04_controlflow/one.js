204eb27c71fd43a3dd49d1506f7f705a86146eea
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2025-01-16 10:36:41 +0000

    flow and switch

---
+++ 04_controlflow/one.js
@@ -0,0 +1,45 @@
+// Control flow in JavaScript refers to the order in which individual statements, 
+// instructions, or function calls are executed or evaluated. 
+// This allows developers to decide how their code should behave in different situations 
+// and create complex and dynamic applications
+//
+//
+// operetors 
+// < , > , <= , >= , == , === , !=
+// && , ||
+
+// code in if else 
+// with diffent condition
+// use && and || operetors
+
+
+// if else with diffrent condition
+
+// let marks = 95
+
+// if (marks >= 90 && marks <= 100){
+//    console.log("cleared with great marks")
+// }
+
+// else if (marks >= 70 && marks < 90){
+//    console.log("cleared with good marks")
+// }
+
+// else if (marks >= 33 && marks < 70){
+//    console.log("cleared");
+// }
+
+// else if (marks >100) {
+//     console.log("worng check again");
+// }
+
+// else {
+//     console.log("needs improvement");
+// }
+
+// // implisite scope
+
+// let grade = 50
+
+// if (grade > 40) console.log("A+")
+//
---
+++ 04_controlflow/switch.js
@@ -0,0 +1,19 @@
+// let month = "april"
+
+// switch (month) {
+//     case "january":
+//         console.log("hi jan");        
+//         break; // breakis imporatant to stop the flow
+//     case "february":
+//         console.log("its fab");        
+//         break; 
+//     case "march":
+//         console.log("its march");        
+//         break;
+//     case "may":
+//         console.log("its may");        
+//         break;
+//     default:
+//         console.log("worng");      
+//         break;
+// }
