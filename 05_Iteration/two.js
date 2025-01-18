598a2f0f364bec87f23e1d2229eebb8a1fe5f26c
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2025-01-18 11:05:31 +0000

    update two js

---
+++ 05_Iteration/two.js
@@ -0,0 +1,29 @@
+// while and do while loop 
+
+// let index = 0
+
+// while (index <= 20) {
+//     console.log(index);
+//     index = index +2
+// }
+
+// print array in while loop
+
+// let stars =["Fenil","Lofer","Bhikho"]
+
+// let index = 0
+
+// while (index < stars.length) {
+//     let result = stars[index];
+//     console.log(result);
+//     index ++
+// }
+
+// print 1 to 20 in do while loop
+
+// let index = 21
+
+// do {
+//     console.log(index);
+//     index ++
+// } while (index <=20 );
