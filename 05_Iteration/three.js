e997615ced00b4dbaf249eccb575fecb21233b6e
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2025-01-20 21:28:46 +0530

    Update three.js

---
+++ 05_Iteration/three.js
@@ -0,0 +1,20 @@
+// for of loop
+
+// The for...of loop in JavaScript is a handy tool for iterating over iterable 
+// objects such as arrays, strings, maps, sets, and more.
+
+// examples of for of loop
+
+// let myarray = [12,23,56,67,78]
+
+// for (const modify of myarray) {
+//     console.log(modify);
+// }
+
+
+// let newarray = "fenil patel"
+
+// for(const newstring of newarray){
+//     console.log(newstring);
+    
+// }
