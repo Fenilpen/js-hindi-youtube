2e02122db256814b732db76e2b30759ec4df5cbb
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2025-01-21 15:32:29 +0000

    four

---
+++ 05_Iteration/four.js
@@ -0,0 +1,38 @@
+// if you want to literate object there is for in loop to use
+
+// let myobj = {
+//     js: "javascript",
+//     py:"python",
+//     rb:"ruby"
+// }
+
+// for (const key in myobj) {
+//  console.log(`${key} is for ${myobj[key]}`);
+// }
+
+// using for in loop in array
+
+// const programming = ["js","rb","py"]
+
+// for (const key in programming) {
+//     console.log(programming[key]);
+    
+// }
+
+// lets try for of in map
+
+// let map = new Map()
+// map.set("IN","India")
+// map.set("FR","France")
+// map.set("GEM","Germeny")
+
+// console.log(map);
+
+// for (const [key,value] of map){
+//     console.log(value);
+// }
+
+
+// for objects for in loop
+// for arrays for of loop
+// for map for of loop
--- 05_Iteration/three.js
+++ 05_Iteration/three.js
@@ -1,20 +1,75 @@
 // for of loop
-
 // The for...of loop in JavaScript is a handy tool for iterating over iterable 
 // objects such as arrays, strings, maps, sets, and more.
 
-// examples of for of loop
+// what is iteration
+// you have a playlist of your favorite songs, and you want to play each song one by one. 
+// Iteration in JavaScript is like a way to go through each song in your playlist. 
+// Instead of manually hitting "next" for each song, you write a loop to do it for you.
 
-// let myarray = [12,23,56,67,78]
 
-// for (const modify of myarray) {
-//     console.log(modify);
+// for of loop example 1 with numbers
+
+// let myarray = [1,2,3,4,5,6,7]
+
+// for (let narray of myarray){
+//     console.log(narray);
 // }
 
+// for of loop example 2 with string
 
-// let newarray = "fenil patel"
+// let mystring = "Fenil Patel"
 
-// for(const newstring of newarray){
+// for (let newstring of mystring){
 //     console.log(newstring);
+// }
+
+// lets jump in to maps
+// check the diffrence 
+
+// let map1 = new Map()
+// console.log(map1);
+
+// let obj = {}
+// console.log(obj);
+
+// console.log(typeof map == obj);
+
+// now iterate the map
+
+// let map = new Map()
+// map.set("IN","India")
+// map.set("FR","France")
+// map.set("GEM","Germeny")
+
+// console.log(map);
+
+
+// lets try for of loop in map
+
+// for (let mapi of map){
+//     console.log(mapi);
+// }
+
+// output is in array
+// but what if we need in string lets try
+// i will use [] syntex
+
+// for (let [key,value] of map){
+//     console.log(key,":-",value);
     
 // }
+
+// now try iterate in object 
+
+// let obj1 = {
+//     game1: "cricket",
+//     game2: "football",
+//     game3: "baseball"
+// }
+
+// for (const myObject of obj1) {
+//     console.log(myObject);
+// }
+
+// obj1 is not iterable
