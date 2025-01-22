bebb15d6a4dbde7addfc7898e666e69302715bbe
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2025-01-22 10:56:06 +0000

    foreach

---
+++ 05_Iteration/five.js
@@ -0,0 +1,42 @@
+// here is the for each method to iterate on array 
+// with diffrent approach
+
+// const coding = ["javascript","python","java","cpp"]
+
+// coding.forEach(function (val) {
+//     console.log(val);
+// } )
+
+// coding.forEach((item) => {
+//     console.log(item);
+// })
+
+// function printMe(item){
+//    console.log(item);
+// }
+
+// coding.forEach(printMe)
+
+// coding.forEach((item, value,index)=>{
+//     console.log(item, value,index);
+// })
+
+// const myCoding = [
+// {
+//     languagename: "javascript",
+//     languageframe: "js"
+// },
+// {
+//     languagename: "java",
+//     languageframe: "java"
+// },
+// {
+//     languagename: "python",
+//     languageframe: "py"
+// }
+// ]
+
+// myCoding.forEach((item)=>{
+//     console.log(item.languagename);
+    
+// })
