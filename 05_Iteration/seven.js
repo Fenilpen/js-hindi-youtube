31eca551662e99913e194d23f285a586194bf6c5
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2025-01-23 17:39:13 +0000

    seven

---
+++ 05_Iteration/seven.js
@@ -0,0 +1,16 @@
+// const myNumbers = [1,2,3,4,5,6,7,8,9,10]
+
+// const newnums = myNumbers.map( (val) => val + 10 )
+
+// console.log(newnums);
+
+
+// Map Nesting using map filter together
+
+const ourNumbers = [1,2,3,4,5,6,7,8,9,10]
+
+// const numss = ourNumbers.map( (val) => val * 10 )
+//                         .map( (num) => num + 1  )
+//                         .filter( (num) => num >= 40)
+   
+// console.log(numss);
