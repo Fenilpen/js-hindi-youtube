64d97826591a291d79ed6161820fc6b586253c30
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2025-01-23 23:50:22 +0530

    Update nine.js

--- 05_Iteration/nine.js
+++ 05_Iteration/nine.js
@@ -1,7 +1,28 @@
-// . reduce 
+// example in . reduce 
 
 // const array = [10,20,30,40,50]
 
-
 // let sum = array.reduce( (acc,curr ) => acc + curr, 0 )
 // console.log(sum);
+
+
+// At the end lets compere three methods foreach, filter, reduce
+
+
+// 1. forEach
+
+// Purpose: Executes a provided function once for each array element.
+// Usage: Useful for performing side effects, such as logging, on each element of an array.
+// Returns: Undefined.
+
+// 2. filter
+
+// Purpose: Creates a new array with all elements that pass the test implemented by the provided function.
+// Usage: Useful for filtering out elements that meet certain criteria.
+// Returns: A new array with elements that pass the test.
+
+// 3. reduce
+
+// Purpose: Executes a reducer function on each element of the array, resulting in a single output value.
+// Usage: Useful for accumulating a value based on the array elements, such as summing numbers or combining objects.
+// Returns: A single value.
