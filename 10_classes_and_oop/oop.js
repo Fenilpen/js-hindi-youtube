73c33ef3ee6b8a5b191d7899d011a6dd81315d88
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2025-02-16 01:27:01 +0530

    Create oop.js

---
+++ 10_classes_and_oop/oop.js
@@ -0,0 +1,38 @@
+// const user = {   // object literal
+//     username: 'fenil',
+//     loginCount: 9,
+//     singnedIn: true,
+
+    // getUserDetails: function(){        
+        // console.log("got user details from database");
+        // console.log(`username: ${this.username}`);
+        // console.log(this);
+        
+        
+    // }
+// }
+
+// console.log(this);
+// console.log(user.username)
+// console.log(user.getUserDetails())
+
+// function user1 (username,logincount,isLoggedIn){
+//     this.username = username;
+//     this.logincount = logincount;
+//     this.isLoggedIn = isLoggedIn;
+
+//     this.greeting = function(){
+//         console.log(`welcome ${this.username}`);
+        
+//     }
+// }
+
+// let detail = new user1("fenil", 10 , true)
+// let detai2 = new user1("fen", 1 , false)
+
+// console.log(detail.constructor);
+// console.log(detai2);
+
+
+// if you put new key new object created you can call instance
+// constructor function will be called becuse of new word
